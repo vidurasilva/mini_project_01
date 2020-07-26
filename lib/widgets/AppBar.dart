9279cef56c2c5ee2f3c6b0887c9ad9f14a05060f
vidurasilva <kvidurachathuranga@gmail.com>
2020-07-26 23:39:40 +0530

    15

--- lib/widgets/AppBar.dart
+++ lib/widgets/AppBar.dart
@@ -31,7 +31,7 @@ class MyAppBar extends StatelessWidget {
             textAlign: TextAlign.start,
           ),
           SizedBox(
-            height: 10,
+            height: 15,
           )
         ],
       ),
